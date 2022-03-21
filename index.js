4fae2d32000a3b1fa52fc3ea021d21320c2bb56a
Karthik Krishna <kkris2000@outlook.com>
2022-03-21 16:51:53 +0530

    function dome get method v1

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+//IIFE
 window.dome = (function(){
+    
     function Dome(els){
 
 
     }
+    
     var dome = {
         get:function(selector){
+            var els;
+            if(typeof selector == "string"){
+                els = document.querySelectorAll(selector);
 
+            } else if(selector.length){
+                els = selector;
+            } else {
+                els = [selector];
+            }
+            return new Dome(els);
         }
     };
 
