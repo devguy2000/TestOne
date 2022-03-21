c4295ccb325249c6a619a389d2950d03dff510ec
Karthik Krishna <kkris2000@outlook.com>
2022-03-21 16:54:42 +0530

    rename

--- dome.js
+++ dome.js
@@ -1,12 +1,17 @@
 //IIFE
 window.dome = (function(){
     
+    // create instance of dome
     function Dome(els){
-
+        for(var i = 0; i< els.length;i++){
+            this[i] = els[i];
+        }
+        this.length = els.length;
 
     }
     
     var dome = {
+        // getting elements
         get:function(selector){
             var els;
             if(typeof selector == "string"){
